3d29b7426a174aff0d8e28bd6ef81dce8f8c0fe1
Mansi <mansipatel7902@gmail.com>
2026-08-13 22:10:18 +0530

    Add edit route and update listing functionality

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
 const Listing = require("./models/listing.js");
 const path = require("path");
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({ extended: true }));
 async function main() {
     await mongoose.connect(process.env.MONGODB_URL);
 }
@@ -18,6 +17,14 @@ main().then(() => {
     console.error(err)
 });
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use((req, res, next) => {
+    console.log("REQUEST:", req.method, req.url);
+    next();
+});
 app.get("/",(req,res) =>{
     res.send("hi! i am root");
 })
@@ -54,6 +61,18 @@ app.post("/listings",async (req,res) =>{
     let {title,description,price,location,country,image} = req.body;
     await new Listing(req.body).save();
     res.redirect("/listings");
+});
+//edit route to edit a listing
+app.get("/listings/:id/edit",async (req,res) =>{
+    const {id}=req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+})
+//update route to update a listing
+app.put("/listings/:id",async (req,res) =>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,req.body.listing);
+    res.redirect(`/listings/${id}`);
 });
 app.listen(port,() =>{
     console.log(`Server is running on port ${port}`);
