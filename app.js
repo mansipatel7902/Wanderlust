dfacd42a037800bcf31a4e10c44dfd49959a1b49
Mansi <mansipatel7902@gmail.com>
2026-08-15 21:08:35 +0530

    add boilerplate and navbar

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-
 const Listing = require("./models/listing.js");
 const path = require("path");
+const ejsMate = require("ejs-mate");
+
 async function main() {
     await mongoose.connect(process.env.MONGODB_URL);
 }
@@ -21,6 +22,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/",(req,res) =>{
     res.send("hi! i am root");
